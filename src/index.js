0581db20f049db029763c9c2065664b091b5dce2
nethask <nethask@gmail.com>
2017-06-09 16:49:56 +0300

    Removed node.js version and tests

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 "use strict"
 
-import WebSocket from "./lib/client/websocket"
+import WebSocket from "./lib/websocket"
 import clientFactory from "./lib/client"
 
 export const Client = clientFactory(WebSocket)
-export {default as Server} from "./lib/server"
